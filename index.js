16fec4a5eb6c8c99ad1ed1860f79b00e195ec3d3
Ashley Wong <ashwong04@gmail.com>
2018-07-08 18:02:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,34 +24,3 @@ function sayHiToGrandma(string){
   if (stringUpperCase === string){ return 'YES INDEED!' }
   if (string === iLoveYou){ return 'I love you, too.' }
 }
-
-
-
-
-// function sayHiToGrandma(string){
-
-//   if (word.toLowerCase === word){
-//     return "I can/'t hear you!"
-//   } else if (word.toUpperCase === word){
-//     return "YES INDEED!"
-//   } else if (word === "I love you, Grandma."){
-//     return "I love you, too."
-//   }
-//   }
-   
-// }
-
-
-// describe('sayHiToGrandma(string)', function() {
-//   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-//     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-//   })
-
-//   it('returns "YES INDEED!" if `string` is uppercase', function() {
-//     expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-//   })
-
-//   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-//     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-//   })
-// })
